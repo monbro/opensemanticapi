2305c9445fd51a3d3c4cd51fe8230e201d89413d
Michael Klein <klein@monbro.de>
2013-06-24 22:32:23 +0100

    bugfix: running out of memory killed node / redis server - added a sleep to be more gently

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 // Don't commit this file to your public repos
 exports.creds = {
   server_port: 8080, // default: 8080
+  sleeptime: 2000, // sleep time in ms between the iteration of all textblocks
   mongoose_auth_local: 'mongodb://localhost/opensemanticapi', // default: mongodb://localhost/opensemanticapi
   min_text_block_length: 120, // default: 120
   debug: true, // default: true
